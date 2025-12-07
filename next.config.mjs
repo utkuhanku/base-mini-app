c2f3d357d4262bd0808f073f3acc2a1d022cbebf
utkuhanku <utkuhank@hotmail.com>
2025-12-07 22:48:40 +0300

    update: mainnet push

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config) => {
+        config.externals.push("pino-pretty", "lokijs", "encoding");
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
